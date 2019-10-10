8adc8c3810954c5224a7cf2a1f06dd2225bde55d
Sofia Sanchez Fuentes <sofia.sanchezf@estudiante.uam.es>
2019-10-10 12:25:24 +0200

    algo preparada para el login

--- p2/app/routes.py
+++ p2/app/routes.py
@@ -37,12 +37,6 @@ def registro():
     # catalogue = json.loads(catalogue_data)
     return render_template('registro.html', title = "Registro")
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-    # print (url_for('static', filename='estilo.css'), file=sys.stderr)
-    # catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogue.json'), encoding="utf-8").read()
-    # catalogue = json.loads(catalogue_data)
-    return render_template('login.html', title = "Login")
 
 @app.route('/busqueda', methods=['GET','POST'])
 def busqueda():
@@ -72,22 +66,16 @@ def busqueda():
     return render_template('index.html', title = "Home", movies=L)
 
 
-    # movies=catalogue['peliculas']
-    #
-    # for item in movies:
-    #     if item['categoria'] == cateogoria:
-    #         return render_template('busqueda.html', title = "Film", film=item)
-    #
-    #
-    # return 'error'
 
-'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
     if 'username' in request.form:
-        # aqui se deberia validar con fichero .dat del usuario
-        if request.form['username'] == 'pp':
+
+        #tendriamos que situarnos en la carpeta que tiene nombre <username>
+        #tendriamos que hacer un fread del fichero datos.dat
+
+        if request.form['username'] == '':
             session['usuario'] = request.form['username']
             session.modified=True
             # se puede usar request.referrer para volver a la pagina desde la que se hizo login
@@ -101,10 +89,9 @@ def login():
         session.modified=True
         # print a error.log de Apache si se ejecuta bajo mod_wsgi
         print (request.referrer, file=sys.stderr)
-        return render_template('login.html', title = "Sign In")
+        return render_template('login.html', title = "Login")
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('usuario', None)
     return redirect(url_for('index'))
-    '''
