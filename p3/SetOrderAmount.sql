b070dcaa2cc45a3c19c445c28baf18ea6d82c2ad
victoria12345 <victoria.pelayo@estudiante.uam.es>
2019-11-21 17:14:33 +0100

    setOrderAmount con funcion

--- p3/SetOrderAmount.sql
+++ p3/SetOrderAmount.sql
@@ -8,3 +8,17 @@ FROM (select orderdetail.orderid as id, sum(price) as s
       where orders.orderid = orderdetail.orderid group by orderdetail.orderid) as sumas
 WHERE
  orders.orderid = sumas.id ;
+
+CREATE OR REPLACE FUNCTION SetOrderAmount() RETURNS void AS $$BEGIN UPDATE orders
+SET
+  totalamount = sumas.s,
+  netamount = sumas.s + tax
+FROM (select orderdetail.orderid as id, sum(price) as s
+      from orderdetail, orders
+      where orders.orderid = orderdetail.orderid group by orderdetail.orderid) as sumas
+WHERE
+ orders.orderid = sumas.id ;
+END;$$
+LANGUAGE plpgpsql;
+
+SELECT * FROM setOrderAmount();
