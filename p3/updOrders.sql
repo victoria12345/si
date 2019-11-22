91516508661535939a722a11b4e58db79832ae25
Sofia <sofia.sanchezf@estudiante.uam.es>
2019-11-22 12:09:16 +0100

    updOrders

---
+++ p3/updOrders.sql
@@ -0,0 +1,47 @@
+DROP TRIGGER IF EXISTS updOrders on orderdetail;
+DROP FUNCTION IF EXISTS updOrdersFunction();
+
+CREATE OR REPLACE FUNCTION updOrdersFunction() RETURNS TRIGGER AS $$ DECLARE result integer;
+  BEGIN
+    IF (TG_OP = 'INSERT') THEN -- SE AÑADE UN ARTICULO AL CARRITO
+      nuevo := NEW.totalamount
+
+      UPDATE orders
+      SET -- queremos cambiar los precios
+        totalamount = netamount + nuevo*(tax/100) + nuevo,
+        netamount = netamount + nuevo
+      WHERE
+          orderid = NEW.orderid
+      RETURN NEW;
+
+    ELSIF (TG_OP = 'UPDATE') THEN -- SE ACTUALIZA UN ARTICULO DEL CARRITO
+      nuevo := NEW.totalamount - OLD.totalamount
+
+      UPDATE orders
+
+      SET
+        totalamount = netamount + nuevo*(tax/100) + nuevo,
+        netamount = netamount + nuevo
+      WHERE
+        orderid = NEW.orderid
+      RETURN NEW;
+    ELSE -- SE ELIMINA UN ARTICULO DEL CARRITO
+      nuevo := - OLD.totalamount
+
+      UPDATE orders
+      SET
+        totalamount = netamount + nuevo*(tax/100) + nuevo,
+        netamount = netamount + nuevo
+      WHERE
+        orderid = OLD.orderid
+      RETURN OLD;
+
+  END IF;
+END;
+
+
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updOrders
+BEFORE INSERT OR UPDATE OR DELETE ON orderdetail
+    FOR EACH ROW EXECUTE PROCEDURE updOrdersFunction();
