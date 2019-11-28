88060110b4e953cd53d9553d6bb18839247384ed
Tu Nombre <you@example.com>
2019-11-28 18:06:05 +0100

    subido nuevo bien

--- p4/sql/clientesDistintos.sql
+++ p4/sql/clientesDistintos.sql
@@ -10,3 +10,11 @@ CREATE INDEX idx_orderdate ON orders(orderdate);
 EXPLAIN SELECT COUNT (DISTINCT customerid)  FROM orders WHERE orders.totalamount >= 100 AND (SELECT extract (month from orderdate)) = 4;
 
 DROP INDEX idx_orderdate;
+
+
+
+
+EXPLAIN SELECT COUNT (DISTINCT customerid)  FROM orders WHERE 
+extract (month from orderdate) = 4 AND 
+extract (year from orderdate) = 2015 and
+orders.totalamount >= 100 ;
